2fd4cb0b7f848df52319d110fc5bd6439a618290
Wellington <wsfin64@gmail.com>
2021-04-06 19:59:51 -0300

    hello world

--- iniciando-projeto/src/main/java/io/github/wsfin64/VendasApplication.java
+++ iniciando-projeto/src/main/java/io/github/wsfin64/VendasApplication.java
@@ -2,12 +2,21 @@ package io.github.wsfin64;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello world!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
+
+
     }
 }
