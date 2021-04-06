f423b7bada8e3c66bf33758731accb6436960838
Wellington <wsfin64@gmail.com>
2021-04-06 19:54:38 -0300

    iniciando o projeto

---
+++ iniciando-projeto/src/main/java/io/github/wsfin64/VendasApplication.java
@@ -0,0 +1,13 @@
+package io.github.wsfin64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
