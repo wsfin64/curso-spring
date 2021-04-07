73490c0b237846e21fb2b1f325706d4be5679eb4
Wellington <wsfin64@gmail.com>
2021-04-06 21:00:41 -0300

    configurations e beans

---
+++ iniciando-projeto/src/main/java/io/github/wsfin64/MinhaConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.wsfin64;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistema de vendas";
+    }
+}
