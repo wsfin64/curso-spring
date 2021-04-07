4a8ccd25bf3b4fd11326a1439f5cf88764cc6584
Wellington <wsfin64@gmail.com>
2021-04-06 21:21:26 -0300

    container ioc

--- iniciando-projeto/src/main/java/io/github/wsfin64/VendasApplication.java
+++ iniciando-projeto/src/main/java/io/github/wsfin64/VendasApplication.java
@@ -4,10 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
+// Especificando explicitamente quais pacotes devem ser escaneados pelo spring
+@ComponentScan (
+        basePackages = {"io.github.wsfin64.repository",
+                        "io.github.wsfin64.service"}
+    )
 @RestController
 public class VendasApplication {
 
