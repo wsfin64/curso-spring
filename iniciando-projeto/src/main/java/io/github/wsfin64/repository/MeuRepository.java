04d0f9a41398730ca41bc7f122acc4cad8015d54
Wellington <wsfin64@gmail.com>
2021-04-06 21:21:26 -0300

    container ioc

---
+++ iniciando-projeto/src/main/java/io/github/wsfin64/repository/MeuRepository.java
@@ -0,0 +1,7 @@
+package io.github.wsfin64.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MeuRepository {
+}
---
+++ iniciando-projeto/src/main/java/io/github/wsfin64/service/MeuServico.java
@@ -0,0 +1,7 @@
+package io.github.wsfin64.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MeuServico {
+}
