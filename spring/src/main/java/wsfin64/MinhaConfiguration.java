b275ec5468f1d62e26503e6edabdb92c70e3f4dd
Wellington <wsfin64@gmail.com>
2021-04-08 16:02:47 -0300

    application properties

--- spring/src/main/java/wsfin64/MinhaConfiguration.java
+++ spring/src/main/java/wsfin64/MinhaConfiguration.java
@@ -6,8 +6,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistema de vendas";
-    }
+
 }
