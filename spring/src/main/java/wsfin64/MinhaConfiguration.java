b8adfa8b64492a2c497d3c9b837da3a7ebc0d315
Wellington <wsfin64@gmail.com>
2021-04-08 16:18:01 -0300

    Multiplus ambientes e profiles

--- spring/src/main/java/wsfin64/MinhaConfiguration.java
+++ spring/src/main/java/wsfin64/MinhaConfiguration.java
@@ -1,10 +1,19 @@
 package wsfin64;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 
 }
